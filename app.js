f8cee5305b2a8653843f9a60b94ca1cf5e504090
Gogopogocat <152077555+Gogopogocat@users.noreply.github.com>
2023-12-25 16:49:01 +0300

    a

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const schedule = require('node-schedule');
+
+const app = express();
+
+const links = process.env.LINKS.split("_")
+console.log(links)
+const job = schedule.scheduleJob('*/13 * * * *', function(){
+  links.forEach(async (link) => {
+    try {
+      const response = await axios.post(link, {"Nebulous":"Pigeon"});
+      console.log(`Successfully posted to ${link}`);
+    } catch (error) {
+      console.error(`Error posting to ${link}: ${error}`);
+    }
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
